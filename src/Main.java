168c8c7a4db87fdfcb4ca18fd9f57aede2e0863f
Aluno <Aluno@LABT03-PC09.ifmg.edu.br>
2024-08-26 10:34:55 -0300

    remove

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,26 @@ public class Main {
         TreeBinary tree = new TreeBinary();
         tree.insert(new Node(30));
         tree.insert(new Node(10));
+        tree.insert(new Node(5));
         tree.insert(new Node(20));
+        tree.insert(new Node(15));
+        tree.insert(new Node(13));
+        tree.insert(new Node(12));
         tree.insert(new Node(40));
-        
+        tree.insert(new Node(50));
+        tree.insert(new Node(60));
+
         tree.preOrder();
+        tree.remove(60);
+
+        System.out.println("==============================");
+        tree.preOrder();
+        tree.remove(12);
+        System.out.println("==============================");
+        tree.preOrder();
+        tree.remove(13);
+        System.out.println("==============================");
+        tree.preOrder();
+
     }
 }
--- src/TreeBinary.java
+++ src/TreeBinary.java
@@ -10,16 +10,14 @@ public class TreeBinary {
      }
 
      private Node insertNewNode(Node newNode, Node current) {
-
           if (current == null)
                return newNode;
-
           if (newNode.getValue() > current.getValue()) {
                current.setRight(insertNewNode(newNode, current.getRight()));
-          } else {
+          }
+          if(newNode.getValue() < current.getValue()){
                current.setLeft(insertNewNode(newNode, current.getLeft()));
           }
-
           return current;
      }
 
@@ -33,5 +31,59 @@ public class TreeBinary {
                preOrder(element.getLeft()); // E
                preOrder(element.getRight()); // D
           }
+     }
+
+     public boolean remove(int value){
+          if(root == null){
+               return false;
+          }else{
+               Node dad;
+               Node noX;
+               //Case 1: nao possui nenhum filho
+               if(root.getValue() != value){
+                    dad = searchDad(root,value);
+                    if(dad.getValue() > value){
+                         dad.setLeft(null);
+                         return true;
+                    }else{
+                         dad.setRight(null);
+                         return true;
+                    }
+               }
+               else{
+                    dad = root;
+                    noX = root;
+               }
+               //Case 2:
+               if(noX.getRight() != null){
+
+               }
+          }
+          return false;
+     }
+
+     public Node searchDad(Node current, int value){
+          if(current == null) return null;
+          if(value > current.getValue()){
+               if(current.getRight() != null){
+                    if(current.getRight().getValue() == value){
+                         return current;
+                    }
+               }
+          }
+          else{
+               if(current.getLeft() != null){
+                    if(current.getLeft().getValue() == value){
+                         return current;
+                    }
+               }
+          }
+          if(current.getValue() > value){
+               return searchDad(current.getLeft(),value);
+          }
+          if(current.getValue() < value){
+               return searchDad(current.getRight(), value);
+          }
+          return null;
      }
 }
