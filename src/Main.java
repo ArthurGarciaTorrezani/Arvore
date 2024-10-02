480d0ff3d8ec607d06535679736671b722ab0a14
Arthur <torrezaniarthur@gmail.com>
2024-10-02 13:23:45 -0300

    falta balanceamento

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,19 @@
 public class Main {
     public static void main(String[] args) {
         TreeBinary tree = new TreeBinary();
-        tree.insert(new Node(30));
-        tree.insert(new Node(10));
-        tree.insert(new Node(40));
+        tree.insert(new Node(2));
+        tree.insert(new Node(1));
+        tree.insert(new Node(3));
 
         //tree.preOrder();
         
-        System.out.println(tree.tamanhoArovre());
-        tree.inverter();
+        System.out.println("=============");
+        tree.posOrder();
+        System.out.println("============");
+        tree.Order();
+        System.out.println("============");
         tree.preOrder();
-
+        System.out.println("============");
+        tree.PosOrderPilha();
     }
 }
--- src/TreeBinary.java
+++ src/TreeBinary.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class TreeBinary {
      private Node root;
 
@@ -10,14 +12,14 @@ public class TreeBinary {
      }
 
      private Node insertNewNode(Node newNode, Node current) {
-          if (current == null)
+          if (current == null) 
                return newNode;
           if (newNode.getValue() > current.getValue()) {
                current.setRight(insertNewNode(newNode, current.getRight()));
           }
-          if(newNode.getValue() < current.getValue()){
+          if (newNode.getValue() < current.getValue()) { 
                current.setLeft(insertNewNode(newNode, current.getLeft()));
-          }
+          } 
           return current;
      }
 
@@ -27,185 +29,219 @@ public class TreeBinary {
 
      private void preOrder(Node element) {
           if (element != null) {
-               System.out.println(element.getValue()); // R
-               preOrder(element.getLeft()); // E
-               preOrder(element.getRight()); // D
+               System.out.println(element.getValue()); 
+               preOrder(element.getLeft()); 
+               preOrder(element.getRight()); 
+          }
+     }
+
+     public void Order() {
+          Order(root);
+     }
+
+     private void Order(Node element) {
+          if (element != null) {
+               preOrder(element.getLeft()); 
+               System.out.println(element.getValue()); 
+               preOrder(element.getRight()); 
+          }
+     }
+
+     public void posOrder() {
+          posOrder(root);
+     }
+
+     private void posOrder(Node element) {
+          if (element != null) {
+              
+               preOrder(element.getLeft()); 
+               preOrder(element.getRight()); 
+               System.out.println(element.getValue()); 
           }
      }
 
      public boolean remove(int value) {
           if (root == null) {
-              return false; // A árvore está vazia
+               return false; // A árvore está vazia
           }
-      
+
           // Caso em que a raiz é o nó a ser removido
           if (root.getValue() == value) {
-              // Se a raiz não tem filhos, simplesmente a remove
-              if (root.getLeft() == null && root.getRight() == null) {
-                  root = null;
-              } else if (root.getLeft() == null) { // Se não tem filho esquerdo
-                  root = root.getRight();
-              } else if (root.getRight() == null) { // Se não tem filho direito
-                  root = root.getLeft();
-              } else { // Caso com dois filhos
-                  Node noDadRightLeft = farLeft(root,root.getRight());
-                  Node substite = noDadRightLeft.getRight();
-                  noDadRightLeft.setLeft(null);
-                  substite.setLeft(root.getLeft());
-                  root = substite; // Substitui a raiz pela substituta
-              }
-              return true; // A raiz foi removida
+               // Se a raiz não tem filhos, simplesmente a remove
+               if (root.getLeft() == null && root.getRight() == null) {
+                    root = null;
+               } else if (root.getLeft() == null) { // Se não tem filho esquerdo
+                    root = root.getRight();
+               } else if (root.getRight() == null) { // Se não tem filho direito
+                    root = root.getLeft();
+               } else { // Caso com dois filhos
+                    Node noDadRightLeft = farLeft(root, root.getRight());
+                    Node substite = noDadRightLeft.getLeft();
+                    noDadRightLeft.setLeft(substite.getRight());
+                    substite.setLeft(root.getLeft());
+                    substite.setRight(root.getRight());
+                    root = substite; // Substitui a raiz pela substituta
+               }
+               return true; // A raiz foi removida
           }
-      
+
           // Para outros casos, procure o pai do nó a ser removido
           Node dad = searchDad(root, value);
           if (dad == null) {
-              return false; // O valor não foi encontrado
+               return false; // O valor não foi encontrado
           }
-      
+
           Node noX = (dad.getValue() < value) ? dad.getRight() : dad.getLeft();
-      
+
           // Caso 1: nó sem filhos
           if (noX.getRight() == null && noX.getLeft() == null) {
-              if (dad.getValue() < value) {
-                  dad.setRight(null);
-              } else {
-                  dad.setLeft(null);
-              }
-              return true;
+               if (dad.getValue() < value) {
+                    dad.setRight(null);
+               } else {
+                    dad.setLeft(null);
+               }
+               return true;
           }
-      
+
           // Caso 2: um filho
           if (noX.getRight() == null) {
-              if (dad.getValue() < value) {
-                  dad.setRight(noX.getLeft());
-              } else {
-                  dad.setLeft(noX.getLeft());
-              }
+               if (dad.getValue() < value) {
+                    dad.setRight(noX.getLeft());
+               } else {
+                    dad.setLeft(noX.getLeft());
+               }
           } else if (noX.getLeft() == null) {
-              if (dad.getValue() < value) {
-                  dad.setRight(noX.getRight());
-              } else {
-                  dad.setLeft(noX.getRight());
-              }
+               if (dad.getValue() < value) {
+                    dad.setRight(noX.getRight());
+               } else {
+                    dad.setLeft(noX.getRight());
+               }
           }
-      
+
           // Caso 3: dois filhos
           if (noX.getRight() != null && noX.getLeft() != null) {
-              Node noDadRightLeft = farLeft(noX,noX.getRight());
-              Node substite = noDadRightLeft.getLeft();
-              noDadRightLeft.setLeft(null);
-              substite.setRight(noX.getRight());
-              substite.setLeft(noX.getLeft());
-              noX.setRight(null);
-              noX.setLeft(null);
-              if (dad.getValue() < value)  dad.setRight(substite);
-               else dad.setLeft(substite);
-              
+               Node noDadRightLeft = farLeft(noX, noX.getRight());
+               Node substite = noDadRightLeft.getLeft();
+               noDadRightLeft.setLeft(substite.getRight());
+               substite.setRight(noX.getRight());
+               substite.setLeft(noX.getLeft());
+               noX.setRight(null);
+               noX.setLeft(null);
+               if (dad.getValue() < value)
+                    dad.setRight(substite);
+               else
+                    dad.setLeft(substite);
           }
-      
-          return true;
-      }
-      
 
-     private Node farLeft(Node dad, Node son){
-          if(son.getLeft() == null){
+          return true;
+     }
+
+     private Node farLeft(Node dad, Node son) {
+          if (son.getLeft() == null) {
                return dad;
           }
           return farLeft(son, son.getLeft());
      }
 
-     public Node searchDad(Node current, int value){
-          if(current == null) return null; 
-          if(value > current.getValue()){
-               if(current.getRight() != null){
-                    if(current.getRight().getValue() == value){
+     public Node searchDad(Node current, int value) {
+          if (current == null)
+               return null;
+          if (value > current.getValue()) {
+               if (current.getRight() != null) {
+                    if (current.getRight().getValue() == value) {
+                         return current;
+                    }
+               }
+          } else {
+               if (current.getLeft() != null) {
+                    if (current.getLeft().getValue() == value) {
                          return current;
                     }
                }
           }
-          else{
-               if(current.getLeft() != null){
-                    if(current.getLeft().getValue() == value){
-                         return current;
-                    }
-               }
+          if (current.getValue() > value) {
+               return searchDad(current.getLeft(), value);
           }
-          if(current.getValue() > value){
-               return searchDad(current.getLeft(),value);
-          }
-          if(current.getValue() < value){
+          if (current.getValue() < value) {
                return searchDad(current.getRight(), value);
           }
           return null;
      }
 
-     // contar numero de nós de uma arvore binaria 1 
-     public int numNos(){
+     // contar numero de nós de uma arvore binaria 1
+     public int numNos() {
           return numNos(root);
      }
-     private int numNos(Node no){
-          if(no == null) return 0;
+
+     private int numNos(Node no) {
+          if (no == null)
+               return 0;
           return numNos(no.getLeft()) + numNos(no.getRight()) + 1;
      }
 
      // contar numero de nós nao-folha 2
-     public int numNosNF(){
+     public int numNosNF() {
           return numNosNF(root);
      }
-     private int numNosNF(Node no){
-          if(no == null) return 0;
-          if(no.getLeft() == null && no.getRight() == null) return 0;
-          return  1 + numNosNF(no.getLeft()) + numNosNF(no.getRight());
+
+     private int numNosNF(Node no) {
+          if (no == null)
+               return 0;
+          if (no.getLeft() == null && no.getRight() == null)
+               return 0;
+          return 1 + numNosNF(no.getLeft()) + numNosNF(no.getRight());
      }
 
-     // conta o número de nós folhas de uma árvore binária. 3
-     public int numNosF(){
+     // conta o número de nós folhas de uma árvore binária 3
+     public int numNosF() {
           return numNosF(root);
      }
-     private int numNosF(Node no){
-          if(no == null) return 0;
-          if(no.getLeft() == null && no.getRight() == null) return 1;
+
+     private int numNosF(Node no) {
+          if (no == null)
+               return 0;
+          if (no.getLeft() == null && no.getRight() == null)
+               return 1;
           return numNosF(no.getLeft()) + numNosF(no.getRight());
      }
 
-     // calcular altura de uma arvore binaria
-     public int tamanhoArovre(){
+     // calcular altura de uma arvore binaria 4
+     public int tamanhoArovre() {
           return tamanhoArovre(root);
      }
-     private int tamanhoArovre(Node no){
-          if(no == null){
+
+     private int tamanhoArovre(Node no) {
+          if (no == null)
                return -1;
-          }
+
           int e = tamanhoArovre(no.getLeft());
           int d = tamanhoArovre(no.getRight());
-          if(e > d){
-               return e  + 1;
-          }
+          if (e > d) return e + 1;
           return d + 1;
      }
 
-     // remover elementos pares
-     public void removerPares(){
+     // remover elementos pares 5
+     public void removerPares() {
           removerPares(root);
      }
-     private void removerPares(Node no){
-          if(no != null){
+
+     private void removerPares(Node no) {
+          if (no != null) {
                removerPares(no.getLeft());
                removerPares(no.getRight());
-               if(no.getValue() % 2 == 0){
+               if (no.getValue() % 2 == 0) {
                     remove(no.getValue());
-               }      
+               }
           }
      }
 
-     // inverter arovre
-     public void inverter(){
+     // inverter arovre 6
+     public void inverter() {
           inverter(root);
      }
 
-     private void inverter(Node no){
-          if(no != null){
+     private void inverter(Node no) {
+          if (no != null) {
                Node noE = no.getLeft();
                Node noD = no.getRight();
                no.setLeft(noD);
@@ -216,32 +252,32 @@ public class TreeBinary {
 
      }
 
-     // 
-     private Node balanceamento(Node no){
-          if(no == null){
+     //
+     private Node balanceamento(Node no) {
+          if (no == null) {
                return null;
           }
 
           Node e = balanceamento(no.getLeft());
           Node d = balanceamento(no.getRight());
 
-          if(d != null && e == null){
-               no.setFator(d.getFator()+1);
+          if (d != null && e == null) {
+               no.setFator(d.getFator() + 1);
           }
-          if(d == null && e != null){
-               no.setFator(e.getFator()+1);
+          if (d == null && e != null) {
+               no.setFator(e.getFator() + 1);
           }
-          if(d != null && e != null){
+          if (d != null && e != null) {
                no.setFator(d.getFator() - e.getFator());
           }
 
-          if(no.getFator() == 2){
-               if(no.getRight().getFator() == 1 || no.getRight().getFator() == 0){
+          if (no.getFator() == 2) {
+               if (no.getRight().getFator() == 1 || no.getRight().getFator() == 0) {
                     Node aux = no.getRight();
                     aux.setLeft(no);
                     no = aux;
                }
-               if(no.getRight().getFator() == -1){
+               if (no.getRight().getFator() == -1) {
                     Node aux = no.getRight().getLeft();
                     aux.setRight(no.getRight());
                     aux.setLeft(no);
@@ -251,13 +287,13 @@ public class TreeBinary {
                }
           }
 
-          if(no.getFator() == -2){
-               if(no.getRight().getFator() == -1 || no.getRight().getFator() == 0){
+          if (no.getFator() == -2) {
+               if (no.getRight().getFator() == -1 || no.getRight().getFator() == 0) {
                     Node aux = no.getLeft();
                     aux.setRight(no);
                     no = aux;
                }
-               if(no.getRight().getFator() == 1){
+               if (no.getRight().getFator() == 1) {
                     Node aux = no.getLeft().getRight();
                     aux.setRight(no);
                     aux.setLeft(no.getLeft());
@@ -268,5 +304,64 @@ public class TreeBinary {
           }
 
           return no;
+     }
+
+     public void OrderPilha(){
+          OrderPilha(root);
+     }
+     //  order pilha
+     private void OrderPilha(Node root) {
+          Stack<Node> pilha = new Stack<>();
+          while (root != null || !pilha.isEmpty()) {
+               if (root != null) {
+                    pilha.push(root);
+                    root = root.getLeft();
+               }else{
+                    root = pilha.pop();
+                    System.out.println(root.getValue());
+                    root = root.getRight();
+               }
+               
+          }
+     }
+
+     public void PreOrderPilha(){
+          PreOrderPilha(root);
+     }
+     // pre order pilha
+     private void PreOrderPilha(Node root) {
+          Stack<Node> pilha = new Stack<>();
+          while (root != null || !pilha.isEmpty()) {
+               if (root != null) {
+                    pilha.push(root);
+                    System.out.println(root.getValue());
+                    root = root.getLeft();
+               }else{
+                    root = pilha.pop();
+                    root = root.getRight();
+               }
+               
+          }
+     }
+
+
+     public void PosOrderPilha(){
+          PosOrderPilha(root);
+     }
+     // pos order pilha
+     private void PosOrderPilha(Node root) {
+          Stack<Node> pilha = new Stack<>();
+          while (root != null || !pilha.isEmpty()) {
+               if (root != null) {
+                    pilha.push(root);
+                    root = root.getRight();
+               }else{
+                    root = pilha.pop();
+                    System.out.println(root.getValue());
+                    root = root.getLeft();
+                  
+               }
+               
+          }
      }
 }
